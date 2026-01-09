918a1241276ad3e4e9f372236b3440716512710b
xd-sarthak <sarthak.sri02032gmail.com>
2026-01-09 19:18:52 +0530

    http-execution added

--- weavy/src/features/executions/components/http-request/executor.ts
+++ weavy/src/features/executions/components/http-request/executor.ts
@@ -1,10 +1,44 @@
 import type { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
+import ky, {type Options as KyOptions} from "ky";
 
-type HttpRequestData = Record<string,unknown>;
+type HttpRequestData = {
+    endpoint?: string;
+    method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
+    body?: string;
+};
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({data,nodeId,context,step}) => {
     //todo pub loading state
-    const result = await step.run(`http-request-${nodeId}`,async () => context) 
-    //todo publish result
+
+    if(!data.endpoint){
+        //TODO: Publish error state
+        throw new NonRetriableError("HTTP node is not configured with an endpoint");
+    }
+
+    const result = await step.run("http-request",async() => {
+        const endpoint = data.endpoint!;
+        const method = data.method || "GET";
+
+        const options:KyOptions = {method};
+
+        if(["POST", "PUT", "PATCH"].includes(method) && data.body) {
+            options.body = data.body;
+        }
+
+        const response = await ky(endpoint,options);
+        const contentType = response.headers.get("content-type");
+        const responseBody = contentType?.includes("application/json") ? await response.json() : await response.text();
+
+        return {
+            ...context,
+            httpResponse: {
+                status: response.status,
+                statusText: response.statusText,
+                data: responseBody
+            }
+        }
+    });
+    
     return result;
 }
