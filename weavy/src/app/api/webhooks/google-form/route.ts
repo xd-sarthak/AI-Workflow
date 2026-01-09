d46cf0d32707fa9e0998c1e2bf5f109e33e16527
xd-sarthak <sarthak.sri02032gmail.com>
2026-01-10 03:22:25 +0530

    google-form-webhook-execution

---
+++ weavy/src/app/api/webhooks/google-form/route.ts
@@ -0,0 +1,38 @@
+import { sendWorkflowExecution } from "@/inngest/utils";
+import { timeStamp } from "console";
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(request: NextRequest){
+    try {
+        const url = new URL(request.url);
+        const workflowId = url.searchParams.get("workflowId");
+        if(!workflowId){
+            return NextResponse.json({message: "Missing query param: workflowId"}, {status: 400});
+        }
+
+        const body = await request.json();
+
+        const formData = {
+            formId: body.formId,
+            formTitle: body.formTitle,
+            responseId: body.responseId,
+            timeStamp: body.timeStamp,
+            respondentEmail: body.respondentEmail,
+            responses: body.responses,
+            raw: body
+        };
+
+        //trigger inngest job
+        await sendWorkflowExecution({
+            workflowId,
+            initialData: {
+                googleForm: formData
+            }
+        });
+
+        
+    } catch (error) {
+        console.error("Google form webhook error: ",error);
+        return NextResponse.json({message: "Internal server error"}, {status: 500});  
+    }
+}
--- weavy/src/features/executions/components/http-request/dialog.tsx
+++ weavy/src/features/executions/components/http-request/dialog.tsx
@@ -15,7 +15,7 @@ const formSchema = z.object({
     variableName: z.string()
                    .min(1,{message: "Variable name is required"})
                    .regex(/^[a-zA-Z_][a-zA-Z0-9_]*$/,{message: "variable name must start with a letter or underscore, and contain only letters, numbers, and underscores"}),   
-    endpoint: z.url({message: "Invalid URL"}),
+    endpoint: z.string().min(1,{message: "Endpoint is required"}),
     method: z.enum(["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"]),
     body: z.string().optional()
 });
